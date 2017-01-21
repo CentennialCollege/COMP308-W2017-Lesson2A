e4400c8bbd082ecf9d53e38bf2a4bd2cd0242b53
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-01-21 12:24:31 -0500

    created demo connect server

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 // our connect server
+let connect = require('connect');
+
+let app = connect();
+const port = 3000;
+
+app.listen(port);
+console.log(`Server listening at port: ${port}`);
+
+// hello route
+app.use('/hello', (req, res, next) => {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("Hello World!");
+  next();
+});
+
+// main route for the root of my website
+app.use('/', (req, res, next) => {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("Welcome!");
+  next();
+});
+
+
